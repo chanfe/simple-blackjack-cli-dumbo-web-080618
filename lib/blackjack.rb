b9a0adc3d8e3e154a1b17d3970e566d6f03dab69
Felix Chan <felixchan29@gmail.com>
2018-07-20 21:36:11 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -47,7 +47,8 @@ end
 
 def invalid_command
   # code invalid_command here
-  puts 
+  puts  "Please enter a valid command"
+  prompt_user
 end
 
 #####################################################
