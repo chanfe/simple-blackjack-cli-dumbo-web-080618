db845aa109cde6f802ba487e2f632ffd768f179a
Felix Chan <felixchan29@gmail.com>
2018-07-20 21:55:42 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -40,9 +40,7 @@ def hit?(card_total)
   # code hit? here
   prompt_user
   user_input = get_user_input
-  
   card_total += deal_card if user_input == 'h'
-  hit? if user_input == 's'
   invalid_command if (user_input != 's' || user_input != 'h')
   card_total
 end
